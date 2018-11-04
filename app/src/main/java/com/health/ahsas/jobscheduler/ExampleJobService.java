0eafee5172e0cb994cd9ecfba6df493d2505ade7
shehryarahmed <shehreyarahmed5@gmail.com>
2018-11-04 16:09:43 +0500

    add service execute when app connected on wifi and charging moode

---
+++ app/src/main/java/com/health/ahsas/jobscheduler/ExampleJobService.java
@@ -0,0 +1,53 @@
+package com.health.ahsas.jobscheduler;
+
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.util.Log;
+
+/**
+ * Created by Admin on 11/4/2018.
+ */
+
+public class ExampleJobService extends JobService {
+
+    private static final String TAG = "ExampleJobService";
+    private boolean jobCancelled = false;
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+
+        Log.d(TAG, "job Started");
+        doBackgroundWork(jobParameters);
+        return true;
+    }
+
+    private void doBackgroundWork(final JobParameters jobParameters) {
+     new Thread(new Runnable() {
+         @Override
+         public void run() {
+            for (int i = 0; i< 10; i++){
+                Log.d(TAG, "run: "+i);
+                if(jobCancelled){
+                    return;
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+             Log.d(TAG, "Job Finished");
+             jobFinished(jobParameters, false);
+         }
+     }).start();
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.d(TAG, "Job cancelled before completion");
+        jobCancelled = true;
+        return true;
+    }
+}
--- app/src/main/java/com/health/ahsas/jobscheduler/MainActivity.java
+++ app/src/main/java/com/health/ahsas/jobscheduler/MainActivity.java
@@ -1,13 +1,67 @@
 package com.health.ahsas.jobscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    Button StartService, StopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartService = (Button) findViewById(R.id.startService);
+        StopService = (Button) findViewById(R.id.stopService);
+
+        StartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService();
+            }
+        });
+
+
+
+        StopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService();
+            }
+        });
+
+    }
+
+    public void startService() {
+        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
+
+        JobInfo info = new JobInfo.Builder(123, componentName)
+//                .setRequiresCharging(false)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        int resultCode = scheduler.schedule(info);
+        if(resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d(TAG, "Job Schedule");
+        }else{
+            Log.d(TAG, "job scheduling failed");
+        }
+    }
+
+    public void stopService() {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "job cancelled");
     }
 }
